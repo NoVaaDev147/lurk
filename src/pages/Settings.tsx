9efc0f18288422bf510c641f3223bde1bae321f4
NoVaaDev147 <frcool87@gmail.com>
2026-04-22 16:01:37 -0400

    v0.1.6 - Lurk is working!

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3,7 +3,7 @@ import { getDb } from "../store/db";
 import { Settings as SettingsType } from "../types";
 import { invoke } from "@tauri-apps/api/core";
 
-const VERSION = "0.1.5";
+const VERSION = "0.1.6";
 
 export default function Settings() {
   const [settings, setSettings] = useState<SettingsType>({
